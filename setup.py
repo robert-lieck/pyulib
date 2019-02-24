672cd72b16ac1cd3118aa6931a5ff60104d6fbc0
Robert Lieck (SilverBullet) <robert.lieck@gmail.com>
2019-02-24 17:33:31 +0100

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='python-util',
+      version='0.1',
+      description='some utility functions',
+      url='https://github.com/robert-lieck/python-util',
+      author='Robert Lieck',
+      # author_email='',
+      # license='',
+      packages=['util'],
+      zip_safe=False,
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      )
