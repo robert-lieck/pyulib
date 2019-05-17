5b2561b0944bac812476c8286f7408366c0618e5
Robert Lieck (SilverBullet) <robert.lieck@gmail.com>
2019-05-17 09:49:27 +0200

    unittests for dict tools

---
+++ util/tests/test_merge_dicts.py
@@ -0,0 +1,36 @@
+from unittest import TestCase
+from util import merge_dicts
+
+
+class TestMerge_dicts(TestCase):
+    def test_merge_dicts(self):
+        d1 = {'1': 1}
+        d2 = {'2': 2, '3': {'x': 1, 'z': 30, 'A': {'B': 'C'}}}
+        d3 = {'2': 20, '3': {'x': 10, 'y': 20, 'A': {'B': 'D', 'E': 'F'}}}
+        # infinite depth
+        self.assertEqual(merge_dicts(d1, d2),
+                         {'1': 1, '2': 2, '3': {'x': 1, 'z': 30, 'A': {'B': 'C'}}})
+        self.assertEqual(merge_dicts(d1, d3),
+                         {'1': 1, '2': 20, '3': {'x': 10, 'y': 20, 'A': {'B': 'D', 'E': 'F'}}})
+        self.assertEqual(merge_dicts(d2, d3),
+                         {'2': 2, '3': {'x': 1, 'y': 20, 'z': 30, 'A': {'B': 'C', 'E': 'F'}}})
+        self.assertEqual(merge_dicts(d1, d2, d3),
+                         {'1': 1, '2': 2, '3': {'x': 1, 'y': 20, 'z': 30, 'A': {'B': 'C', 'E': 'F'}}})
+        # depth = 1
+        self.assertEqual(merge_dicts(d1, d2, depth=1),
+                         {'1': 1, '2': 2, '3': {'x': 1, 'z': 30, 'A': {'B': 'C'}}})
+        self.assertEqual(merge_dicts(d1, d3, depth=1),
+                         {'1': 1, '2': 20, '3': {'x': 10, 'y': 20, 'A': {'B': 'D', 'E': 'F'}}})
+        self.assertEqual(merge_dicts(d2, d3, depth=1),
+                         {'2': 2, '3': {'x': 1, 'y': 20, 'z': 30, 'A': {'B': 'C'}}})
+        self.assertEqual(merge_dicts(d1, d2, d3, depth=1),
+                         {'1': 1, '2': 2, '3': {'x': 1, 'y': 20, 'z': 30, 'A': {'B': 'C'}}})
+        # depth = 0
+        self.assertEqual(merge_dicts(d1, d2, depth=0),
+                         {'1': 1, '2': 2, '3': {'x': 1, 'z': 30, 'A': {'B': 'C'}}})
+        self.assertEqual(merge_dicts(d1, d3, depth=0),
+                         {'1': 1, '2': 20, '3': {'x': 10, 'y': 20, 'A': {'B': 'D', 'E': 'F'}}})
+        self.assertEqual(merge_dicts(d2, d3, depth=0),
+                         {'2': 2, '3': {'x': 1, 'z': 30, 'A': {'B': 'C'}}})
+        self.assertEqual(merge_dicts(d1, d2, d3, depth=0),
+                         {'1': 1, '2': 2, '3': {'x': 1, 'z': 30, 'A': {'B': 'C'}}})
